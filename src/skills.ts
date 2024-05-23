0ef961d8359925f1e57cd57cd0bbc73b264cea1d
Swastik Agarwala <agarwalaswastik@gmail.com>
2024-05-23 19:44:15 +0530

    Add an overlay over the skills section to indicate correlation of percentage to proficiency level
    + Add sky blue color as an accent color for the overlay
    + For the overlay, add headers and pseudoelements with a border that stretch through till the end of the section

--- src/skills.ts
+++ src/skills.ts
@@ -1,4 +1,11 @@
 const setSkillLevels = () => {
+  const overlayElements = document.querySelectorAll(".skills__overlay *");
+  overlayElements.forEach((elem) => {
+    if (elem instanceof HTMLElement) {
+      elem.style.left = elem.dataset.level ?? "0";
+    }
+  });
+
   const skillBars = document.querySelectorAll(".skill__bar");
   skillBars.forEach((skillBar) => {
     if (skillBar instanceof HTMLElement) {
