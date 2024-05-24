32c86ab45e06b4c803c2416af070b008408f3f0c
Swastik Agarwala <agarwalaswastik@gmail.com>
2024-05-25 01:14:17 +0530

    Add markup and styles for projects section
    + Add list of projects, each consisting of a title, list of links, description, a list of tags, and an image
    + Layout the project cards in a responsive grid with 1 column on smaller screens and 2 columns on bigger screens

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import "modern-normalize";
 import "/styles/base.css";
+import "/styles/components/projects.css";
 import "/styles/components/skills.css";
 import "/styles/components/contact.css";
 import "/styles/utils.css";
