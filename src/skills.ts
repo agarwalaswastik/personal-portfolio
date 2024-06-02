a9bf4f18af483b94ea0849c3af10e5a2898a9ec9
Swastik Agarwala <agarwalaswastik@gmail.com>
2024-06-02 17:41:09 +0530

    Clear some TODOes
    + Add Skill percentages
    + Add Vendor Prefixes

--- src/skills.ts
+++ src/skills.ts
@@ -1,16 +1,14 @@
 const setupSkills = () => {
   const overlayElements = document.querySelectorAll(".skills__overlay *");
   overlayElements.forEach((elem) => {
-    if (elem instanceof HTMLElement) {
-      elem.style.left = elem.dataset.level ?? "0";
-    }
+    const htmlElem = elem as HTMLElement;
+    htmlElem.style.left = htmlElem.dataset.level ?? "0";
   });
 
   const skillBars = document.querySelectorAll(".skill__bar");
   skillBars.forEach((skillBar) => {
-    if (skillBar instanceof HTMLElement) {
-      skillBar.style.width = skillBar.dataset.level ?? "0";
-    }
+    const htmlSkillBar = skillBar as HTMLElement;
+    htmlSkillBar.style.width = htmlSkillBar.dataset.level ?? "0";
   });
 };
 
